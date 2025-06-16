bea178a41c2d838999b16ed7dbe80f90ba37bb60
Maurício3553 <mauricio.under@gmail.com>
2025-06-16 20:53:37 -0300

    Adicionado operadores de comparação.

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min)
 
-    alert(result)
+    if (min >= max) {
+        alert("O valor mínimo deve ser MENOR que o máximo.")
+
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1) + min)
+
+        alert(result)
+    }
+
+
+
 }
